e5f56f40109b38764f476ccdfa5b96239425ecb8
Tobi <tobias.wagner91@gmail.com>
2021-05-22 14:40:24 +0200

    Add IngredientForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IngredientFormComponent } from './ingredient-form/ingredient-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngredientFormComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ingredient-form/ingredient-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient-form',
+  templateUrl: './ingredient-form.component.html',
+  styleUrls: ['./ingredient-form.component.css']
+})
+export class IngredientFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
