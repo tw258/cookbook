cc74d63c16fbc956dbaec2313d9d5b2fe1e3f603
tobi <tobias.wagner91@gmail.com>
2021-06-21 17:03:28 +0200

    add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IngredientFormComponent } from './ingredient-form/ingredient-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, IngredientFormComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
