2725b13b709ef5da1852d62a3d4865efa57e7bfd
tobi <tobias.wagner91@gmail.com>
2021-06-21 19:16:19 +0200

    recipe list dummy

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Difficulty, Measurement, Recipe } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    {
+      title: 'Spaghetti Bolognese',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
+      preparationTimeInMinutes: 20,
+      difficulty: Difficulty.Easy,
+      parameterizedIngredients: [
+        {
+          amount: 1,
+          ingredient: 'Eier',
+          measurement: Measurement.Pieces,
+        },
+      ],
+    },
+    {
+      title: 'Spaghetti Bolognese',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
+      preparationTimeInMinutes: 20,
+      difficulty: Difficulty.Easy,
+      parameterizedIngredients: [
+        {
+          amount: 1,
+          ingredient: 'Eier',
+          measurement: Measurement.Pieces,
+        },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
