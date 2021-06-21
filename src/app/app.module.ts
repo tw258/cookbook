47799d6fa55187e6b1c3e23a1d720ecc79fa3644
tobi <tobias.wagner91@gmail.com>
2021-06-21 17:54:00 +0200

    add recipe-forms component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { Route, RouterModule } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
-const routes: Route[] = [{ path: '', component: RecipeListComponent }];
+const routes: Route[] = [
+  { path: '', component: RecipeListComponent },
+  { path: 'new-recipe', component: RecipeFormComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, IngredientFormComponent],
+  declarations: [AppComponent, IngredientFormComponent, RecipeFormComponent],
   imports: [BrowserModule, BrowserAnimationsModule, SharedModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
