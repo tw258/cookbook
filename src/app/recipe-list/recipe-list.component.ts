2e7632165e99ab08beb55e558024a9d293d7e59a
tobi <tobias.wagner91@gmail.com>
2021-06-24 20:01:10 +0200

    recipe list finalise

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Difficulty, Measurement, Recipe } from '../recipe.service';
+import { Difficulty, Measurement, Recipe, RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,36 +7,23 @@ import { Difficulty, Measurement, Recipe } from '../recipe.service';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    {
-      title: 'Spaghetti Bolognese',
-      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
-      preparationTimeInMinutes: 20,
-      difficulty: Difficulty.Easy,
-      parameterizedIngredients: [
-        {
-          amount: 1,
-          ingredient: 'Eier',
-          measurement: Measurement.Pieces,
-        },
-      ],
-    },
-    {
-      title: 'Spaghetti Bolognese',
-      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
-      preparationTimeInMinutes: 20,
-      difficulty: Difficulty.Easy,
-      parameterizedIngredients: [
-        {
-          amount: 1,
-          ingredient: 'Eier',
-          measurement: Measurement.Pieces,
-        },
-      ],
-    },
-  ];
+  recipes: Recipe[] = this.recipeservice.recipes;
 
-  constructor() {}
+  constructor(private recipeservice: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.recipeservice.addRecipe({
+    //   title: 'Pizza',
+    //   note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
+    //   preparationTimeInMinutes: 30,
+    //   difficulty: Difficulty.Medium,
+    //   parameterizedIngredients: [
+    //     {
+    //       amount: 1,
+    //       ingredient: 'Eier',
+    //       measurement: Measurement.Pieces,
+    //     },
+    //   ],
+    // });
+  }
 }
