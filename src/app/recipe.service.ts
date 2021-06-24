1c185a3e5bfdc4b27dcd1fe85d334a1282634658
tobi <tobias.wagner91@gmail.com>
2021-06-24 20:01:39 +0200

    add recipe dummy list

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -37,10 +37,43 @@ export interface Recipe {
   providedIn: 'root',
 })
 export class RecipeService {
-  private recipes: Recipe[] = [];
+  private _recipes: Recipe[] = [
+    {
+      id: nanoid(),
+      title: 'Spaghetti Bolognese',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
+      preparationTimeInMinutes: 20,
+      difficulty: Difficulty.Easy,
+      parameterizedIngredients: [
+        {
+          amount: 1,
+          ingredient: 'Eier',
+          measurement: Measurement.Pieces,
+        },
+      ],
+    },
+    {
+      id: nanoid(),
+      title: 'Spaghetti Bolognese',
+      note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
+      preparationTimeInMinutes: 20,
+      difficulty: Difficulty.Easy,
+      parameterizedIngredients: [
+        {
+          amount: 1,
+          ingredient: 'Eier',
+          measurement: Measurement.Pieces,
+        },
+      ],
+    },
+  ];
+
+  get recipes() {
+    return this._recipes;
+  }
 
   addRecipe(recipe: Recipe) {
-    this.recipes.push({
+    this._recipes.push({
       ...recipe,
       id: nanoid(),
     });
