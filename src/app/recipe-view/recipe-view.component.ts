62e73ecba74a28febe5f6f889d9aec9ef8ee86f6
tobi <tobias.wagner91@gmail.com>
2021-06-28 18:44:00 +0200

    add items

--- src/app/recipe-view/recipe-view.component.ts
+++ src/app/recipe-view/recipe-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { nanoid } from 'nanoid';
+import { Difficulty, Measurement, Recipe } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-view',
@@ -7,6 +8,25 @@ import { Recipe } from '../recipe.service';
   styleUrls: ['./recipe-view.component.css'],
 })
 export class RecipeViewComponent implements OnInit {
+  @Input() recipe: Recipe = {
+    id: nanoid(),
+    title: 'Spaghetti Bolognese',
+    note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
+    preparationTimeInMinutes: 20,
+    difficulty: Difficulty.Easy,
+    parameterizedIngredients: [
+      {
+        amount: 1,
+        ingredient: 'Eier',
+        measurement: Measurement.Stck,
+      },
+      {
+        amount: 2,
+        ingredient: 'Milch',
+        measurement: Measurement.l,
+      },
+    ],
+  };
   constructor() {}
 
   ngOnInit(): void {}
