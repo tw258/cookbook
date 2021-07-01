8e06d9f9e16dc671ebb1d70c26bf92c94757f7ab
Tobi <tobias.wagner91@gmail.com>
2021-07-01 14:57:31 +0200

    add author chip

--- src/app/recipe.service.ts
+++ src/app/recipe.service.ts
@@ -67,7 +67,6 @@ export class RecipeService {
       id: '2',
       creationDateAsIsoString: '2021-06-29T19:20:11.643Z',
       title: 'Pizza',
-      author: 'Oscar96',
       note: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum...',
       preparationTimeInMinutes: 40,
       imagesAsBase64: [
