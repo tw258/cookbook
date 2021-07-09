d2c118a78827b130ddd3d2b825d8b939b0669967
tobi <tobias.wagner91@gmail.com>
2021-07-09 20:08:12 +0200

    remove garbage

--- src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { SlideshowModule } from 'ng-simple-slideshow';
-import { MaterialModule } from './material/material.module';
-
-@NgModule({
-  exports: [MaterialModule, CommonModule, SlideshowModule],
-})
-export class SharedModule {}
