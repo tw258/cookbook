45c03da72d32a1eaf75fd4c719f5dcdbab59a522
tobi <tobias.wagner91@gmail.com>
2021-07-29 17:58:45 +0200

    my masterchange

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-//new feature
+//my masterchange
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch(err => console.error(err));
