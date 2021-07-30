46295a6fab3dad1e08f76078bf8f74d5973ec04e
Tobi <tobias.wagner91@gmail.com>
2021-07-30 20:45:12 +0200

    add login component

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+  invalidLogin = false;
+
+  constructor(private login: LoginService, private router: Router) {}
+  ngOnInit(): void {
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+    if (username && password) {
+      this.login.getLogin(username, password).subscribe(() => this.router.navigate(['/recipes']));
+    }
+  }
+
+  handleLogin() {
+    this.login.getLogin(this.username, this.password).subscribe(
+      () => this.router.navigate(['/recipes']),
+      () => (this.invalidLogin = true),
+    );
+  }
+}
