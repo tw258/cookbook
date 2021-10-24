7b74639c6e67e02980ba06c8da1e0e9b298f7663
Oscar Rosner <oscar.rosner@web.de>
2021-10-24 14:04:18 +0200

    specify image routes

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const Mongodb = require('../mongodb');
 const mongodb = new Mongodb();
 
-// Image routes here...
+// Image routes here:
+// - GET      /images?recipe=<recipe-id>   Get all images of a recipe.
+// - GET      /images/<image-id>           Get an existing image.
+// - POST     /images?recipe=<recipe-id>   Insert a new image and add its ID to the existing recipe.
+// - DELETE   /images/<image-id>           Delete an image.
 
 module.exports = router;
