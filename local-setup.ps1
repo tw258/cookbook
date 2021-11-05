53ffac20c9aa073b4d243a04898f549d45ebe94d
Oscar Rosner <oscar.rosner@web.de>
2021-11-05 07:07:48 +0100

    update .env

--- local-setup.ps1
+++ local-setup.ps1
@@ -11,9 +11,13 @@ $orangeCheck = @{
 Write-Host -NoNewline "(1/3) Creating '.env' file..."
 
 if (!(Test-Path -Path ".env")) {
-    Copy-Item .env.example ".env" *>$null
+    $ENV_EXAMPLE_CLEAN = (Get-Content .env.example) -replace '^\s?#.*', '' | Where-Object {$_.trim() -ne "" }
+    $ENV_HEADER = "# Add your credentials to the variables below.`n# They will not be shared with anyone.`n"
+    $ENV_HEADER | Add-Content .\.env
+    $ENV_EXAMPLE_CLEAN | Add-Content .\.env
+
     Write-Host @greenCheck
-    Write-Host -ForegroundColor Yellow "      - Please add your MongoDB credentials to '.env'!"
+    Write-Host -ForegroundColor Yellow "      - Please add your credentials to '.env'!"
 } else {
     Write-Host @orangeCheck
     Write-Host "      - file already exists, skipping this step"
