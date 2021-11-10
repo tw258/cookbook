91d29736ebe1d697a617ff0e0abc8b9bd6583bef
tobi <tobias.wagner91@gmail.com>
2021-11-10 17:15:24 +0100

    introduce history

--- backend/dbmigrations.js
+++ backend/dbmigrations.js
@@ -9,19 +9,39 @@ const DB_URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@192.168.2.5:27017/cookbo
 const RECIPES_COLLECTION = 'recipes';
 const IMAGES_COLLECTION = 'images';
 const USERS_COLLECTION = 'users';
+const recipesCollection = db.collection(RECIPES_COLLECTION);
+const imageCollection = db.collection(IMAGES_COLLECTION);
+const usersCollection = db.collection(USERS_COLLECTION);
 
 async function migrateDB() {
   const client = new MongoClient(DB_URL);
 
   await client.connect();
   const db = client.db();
-  const collection = db.collection(RECIPES_COLLECTION);
-  const imageCollection = db.collection(IMAGES_COLLECTION);
 
   //Do stuff here
+  //await migrate2();
 
+  client.close();
+}
+migrateDB();
+
+//10.11.2021 hash passwords of all users
+async function migrate1() {
+  const allUsers = await usersCollection.find().toArray();
+  for (const user of allUsers) {
+    //hash password
+    user.password = hashPassword(user.password);
+
+    console.log(user);
+
+    await collection.replaceOne({ _id: user._id }, user);
+  }
+}
+
+//10.11.2021 add isThumbnail property to first images
+async function migrate2() {
   const allRecipes = await collection.find().toArray();
-
   for (const currentRezept of allRecipes) {
     if (currentRezept.imageIds.length > 0) {
       await imageCollection.updateOne(
@@ -30,18 +50,4 @@ async function migrateDB() {
       );
     }
   }
-
-  // const allUsers = await collection.find().toArray();
-  // for (const user of allUsers) {
-  //   //hash password
-  //   user.password = hashPassword(user.password);
-
-  //   console.log(user);
-
-  //   await collection.replaceOne({ _id: user._id }, user);
-  // }
-
-  client.close();
 }
-
-migrateDB();
