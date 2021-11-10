1de4597030c00bb06b76ba5254f20fa0478face4
tobi <tobias.wagner91@gmail.com>
2021-11-10 19:03:33 +0100

    add confirm-password-change-dialog

---
+++ frontend/src/app/change-password/confirm-password-change-dialog/confirm-password-change-dialog.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-password-change-dialog',
+  templateUrl: './confirm-password-change-dialog.component.html',
+  styleUrls: ['./confirm-password-change-dialog.component.css'],
+})
+export class ConfirmPasswordChangeDialogComponent {
+  constructor() {}
+
+  onCancel() {}
+
+  onConfirm() {}
+}
