ca6d858b12d73ebc201d6cd18d4946a0088f665f
tobi <tobias.wagner91@gmail.com>
2021-11-10 17:11:41 +0100

    add isThumbnail update

--- backend/dbmigrations.js
+++ backend/dbmigrations.js
@@ -4,7 +4,7 @@ const hashPassword = require('./utils/crypto');
 
 const DB_USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME;
 const DB_PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
-const DB_URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@localhost:27017/cookbook?authSource=admin`; // Development: backend runs directly on host machine.
+const DB_URL = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@192.168.2.5:27017/cookbook?authSource=admin`; // Development: backend runs directly on host machine.
 
 const RECIPES_COLLECTION = 'recipes';
 const IMAGES_COLLECTION = 'images';
@@ -15,19 +15,31 @@ async function migrateDB() {
 
   await client.connect();
   const db = client.db();
-  const collection = db.collection(USERS_COLLECTION);
+  const collection = db.collection(RECIPES_COLLECTION);
+  const imageCollection = db.collection(IMAGES_COLLECTION);
 
   //Do stuff here
-  const allUsers = await collection.find().toArray();
 
-  for (const user of allUsers) {
-    //hash password
-    user.password = hashPassword(user.password);
+  const allRecipes = await collection.find().toArray();
 
-    console.log(user);
-
-    await collection.replaceOne({ _id: user._id }, user);
+  for (const currentRezept of allRecipes) {
+    if (currentRezept.imageIds.length > 0) {
+      await imageCollection.updateOne(
+        { _id: currentRezept.imageIds[0] },
+        { $set: { isThumbnail: true } },
+      );
+    }
   }
+
+  // const allUsers = await collection.find().toArray();
+  // for (const user of allUsers) {
+  //   //hash password
+  //   user.password = hashPassword(user.password);
+
+  //   console.log(user);
+
+  //   await collection.replaceOne({ _id: user._id }, user);
+  // }
 
   client.close();
 }
