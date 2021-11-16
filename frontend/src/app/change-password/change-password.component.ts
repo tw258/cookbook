daa503b72995d23fb580d8bcb8a418dcd0646365
tobi <tobias.wagner91@gmail.com>
2021-11-16 19:19:24 +0100

    add dialogbox

--- frontend/src/app/change-password/change-password.component.ts
+++ frontend/src/app/change-password/change-password.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { UserService } from '../user.service';
+import { ConfirmPasswordChangeDialogComponent } from './confirm-password-change-dialog/confirm-password-change-dialog.component';
 
 @Component({
   selector: 'app-change-password',
@@ -8,13 +12,35 @@ import { UserService } from '../user.service';
 })
 export class ChangePasswordComponent {
   newPassword = '';
+  isPasswordVisible = false;
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private matDialog: MatDialog,
+    private matSnackbar: MatSnackBar,
+    private router: Router,
+  ) {}
+
+  onToggleVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  private updatePassword() {
+    this.userService.updatePassword(this.newPassword).subscribe({
+      complete: () => {
+        this.matSnackbar.open('Passwort erfolgreich geändert', 'OK', { duration: 3000 });
+        this.router.navigateByUrl('/recipes');
+      },
+      error: err => console.log(err),
+    });
+  }
 
   handleChangePassword() {
-    this.userService.updatePassword(this.newPassword).subscribe(
-      x => console.log('sucessfully changed'),
-      err => console.log(err),
-    );
+    this.matDialog.open(ConfirmPasswordChangeDialogComponent, {
+      data: {
+        onConfirm: () => this.updatePassword(),
+        onCancel: () => {}, //we ignore Cancel
+      },
+    });
   }
 }
--- frontend/src/app/change-password/confirm-password-change-dialog/confirm-password-change-dialog.component.ts
+++ frontend/src/app/change-password/confirm-password-change-dialog/confirm-password-change-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-password-change-dialog',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirm-password-change-dialog.component.css'],
 })
 export class ConfirmPasswordChangeDialogComponent {
-  constructor() {}
+  constructor(
+    private matDialogRef: MatDialogRef<ConfirmPasswordChangeDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: { onCancel: () => void; onConfirm: () => void },
+  ) {}
 
-  onCancel() {}
+  onCancel() {
+    this.data.onCancel();
+    this.matDialogRef.close();
+  }
 
-  onConfirm() {}
+  onConfirm() {
+    this.data.onConfirm();
+    this.matDialogRef.close();
+  }
 }
